e2dc71b68c80973ce2f756db123ff3887661fb0d
CoderGuy614 <jlutz.110@gmail.com>
2020-07-08 19:20:07 +0800

    Create React App - Client

--- routes/test.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { testGetFunction, testPostFunction } = require("../controllers/test");
-
-router.get("/", testGetFunction);
-router.post("/", testPostFunction);
-
-module.exports = router;
--- validator/userSignup.js
+++ validator/userSignup.js
@@ -1,23 +1,5 @@
 const { body, validationResult } = require("express-validator");
 
-// exports.userSignupValidator = (req, res, next) => {
-//   const { check, validationResult } = require("express-validator");
-//   req.check("name", "Name is required").notEmpty();
-//   req.check("email", "Email is required").isEmail();
-//   req.check("password", "Password is required").notEmpty();
-//   req
-//     .check("password")
-//     .isLength({ min: 6 })
-//     .withMessage("Password must be at least 6 characters");
-
-//   const errors = req.validationErrors();
-//   if (errors) {
-//     const firstErrorMessage = errors[0].message;
-//     return res.status(400).json({ error: firstErrorMessage });
-//   }
-//   next();
-// };
-
 const userSignupValidator = () => {
   return [body("name").notEmpty(), body("password").isLength({ min: 5 })];
 };
